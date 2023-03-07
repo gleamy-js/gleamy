2e653e2d1a6fa2af8dfeb9783a0996b5e812f38e
Space Corp Engineering <development@spacecorp.nl>
2023-03-07 21:23:34 +0100

    fix: glitter alpha property was mixed up with intensity property

--- src/lib/definitions/glitter/glitter.ts
+++ src/lib/definitions/glitter/glitter.ts
@@ -2,7 +2,7 @@ import Color from 'color';
 import { ParticleDefinition } from '../../../types';
 
 const round = (value: number): number => {
-  return Math.round(value * 100) / 100;
+  return Math.floor(value * 100) / 1000;
 };
 
 export const glitter = ({
@@ -14,13 +14,16 @@ export const glitter = ({
   depthAlpha,
   intensity,
 }: ParticleDefinition): void => {
-  const alpha = depthAlpha ? round(depthPercentage) : 1;
-  const phase = round(
-    intensity *
-      Math.sin(
-        ((depthPercentage % 2 === 0 ? pos1X : pos1Y) / 100) * Math.PI * alpha,
-      ),
-  );
+  const alpha = depthAlpha ? round(depthPercentage) : 0.5;
+  const phase =
+    10 *
+    round(
+      intensity *
+        Math.sin(
+          ((depthPercentage % 2 === 0 ? pos1X : pos1Y) / 1000) * Math.PI,
+        ),
+    );
+
   let color = Color('black');
 
   if (particleColor === 'holographic') {
@@ -31,14 +34,16 @@ export const glitter = ({
     color = Color('blue')
       .rotate(rotation * 360)
       .lighten(phase > 0 ? phase : 0)
-      .darken(phase < 0 ? -phase : 0)
-      .fade(-alpha);
+      .darken(phase < 0 ? -phase : 0);
   } else {
     color = Color(particleColor)
       .darken(phase < 0 ? -phase : 0)
-      .lighten(phase > 0 ? phase : 0)
-      .fade(-alpha);
+      .lighten(phase > 0 ? phase : 0);
   }
 
-  context.fillStyle = color.string();
+  if (depthAlpha) {
+    context.fillStyle = color.fade(alpha).string();
+  } else {
+    context.fillStyle = color.string();
+  }
 };
