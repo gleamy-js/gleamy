563c25b8f70dbf511b7e78475a9cd4da4a9d65da
Jordy Houtman <jhoutman@anwb.nl>
2024-04-10 16:11:21 +0200

    fix: oopsie on not including dist/ folder

--- packages/gleamy/scripts/postpack.cjs
+++ packages/gleamy/scripts/postpack.cjs
@@ -6,6 +6,9 @@ const result = JSON.parse(fs.readFileSync(appRoot + '/entitiesToPack.json'));
 console.log('Gleamy: cleaning up.');
 try {
   result.entitiesToPack.forEach((entity) => {
+    if (entity === "dist") {
+      return void(0);
+    }
     fs.rmSync(`../../packages/gleamy/${entity}`, { recursive: true });
   });
 } catch (error) {
